135e91145691cc21995219261168539bacad7ade
TinaTech-Consulting <>
2024-12-05 08:09:23 +0200

    footer

--- src/app/(main)/components/QualityAndSafety.js
+++ src/app/(main)/components/QualityAndSafety.js
@@ -7,7 +7,7 @@ function QualityAndSafety() {
   return (
     <div className="bg-scroll bg-[url('/new.avif')] bg-cover bg-no-repeat w-full md:min-h-screen md:h-[400px] h-[600px] relative">
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black to-transparent opacity-95">
-        <div className="flex flex-col items-center justify-center text-white z-50 py-10 md:mt-10 mx-auto">
+        <div className="flex flex-col items-center justify-center text-white z-50 pt-10 md:mt-10 mx-auto">
           <motion.h1
             initial={{
               y: 100,
@@ -36,7 +36,7 @@ function QualityAndSafety() {
               opacity: 1,
             }}
             transition={{ duration: 1.8 }}
-            className="text-center px-4 md:px-32 my-10"
+            className="text-center px-4 md:px-32 my-5 md:my-10"
           >
             In aircraft maintenance, Safety, Quality, and Commitment are key to
             ensuring reliability. Safety comes first with strict inspections and
@@ -74,7 +74,7 @@ function QualityAndSafety() {
               opacity: 1,
             }}
             transition={{ duration: 1.8 }}
-            className="text-center px-4 md:px-32 my-10"
+            className="text-center px-4 md:px-32 my-5 md:my-10"
           >
             Building a global footprint in aircraft maintenance means expanding
             our reach and expertise across the world. By establishing a network
